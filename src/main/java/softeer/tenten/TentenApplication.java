c17ee315db4ce4eb066e517c62fe8cf9e901ad8f
Hymin2 <khm8246@gmail.com>
2024-01-04 20:16:03 +0900

    chore: 환경 설정

---
+++ src/main/java/softeer/tenten/TentenApplication.java
@@ -0,0 +1,13 @@
+package softeer.tenten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TentenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TentenApplication.class, args);
+	}
+
+}
