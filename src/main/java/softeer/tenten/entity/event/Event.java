b9c6c81c05cea5d8d7469a6b56bb8b863650075b
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2024-01-06 12:58:13 +0900

    feat: event entity 생성

---
+++ src/main/java/softeer/tenten/entity/event/Event.java
@@ -0,0 +1,32 @@
+package softeer.tenten.entity.event;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "code", nullable = false)
+    private String code;
+}
