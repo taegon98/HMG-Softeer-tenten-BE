b698b7825664deb3f4f1c0223d49fa3b066a5fb3
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2024-01-07 15:57:17 +0900

    feat: ReviewResponse 생성
    - ReviewList 응답 inner class 생성

---
+++ src/main/java/softeer/tenten/dto/popup/review/ReviewResponse.java
@@ -0,0 +1,20 @@
+package softeer.tenten.dto.popup.review;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+public class ReviewResponse {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class ReviewList {
+
+        private final String title;
+        private final String destination;
+        private final String date;
+        private final String content;
+        private final String image;
+    }
+}
