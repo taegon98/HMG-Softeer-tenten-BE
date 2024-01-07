7297de0b2031e1013bf1e5bc42cdcfe2242fc576
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2024-01-07 16:03:41 +0900

    fix: ReviewController mapping URI 수정

--- src/main/java/softeer/tenten/controller/popup/review/ReviewController.java
+++ src/main/java/softeer/tenten/controller/popup/review/ReviewController.java
@@ -2,6 +2,8 @@ package softeer.tenten.controller.popup.review;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import softeer.tenten.dto.popup.review.ReviewResponse;
@@ -17,7 +19,8 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
-    public ResponseEntity<Object> getReviews() {
+    @GetMapping("/pop-up/{id}/reviews")
+    public ResponseEntity<Object> getReviews(@PathVariable Long id) {
         List<ReviewResponse.ReviewList> reviews = reviewService.getReviewList();
         return ResponseEntity.ok(ApiResponse.onSuccess(reviews));
     }
