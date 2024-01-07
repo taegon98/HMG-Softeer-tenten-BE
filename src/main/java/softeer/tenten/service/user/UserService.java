3c3e6846448a28414c83f0f3d72228e92eba3470
Hymin2 <khm8246@gmail.com>
2024-01-07 12:56:53 +0900

    refactor: UserService에서 User 조회를 함수로 분리

--- src/main/java/softeer/tenten/service/user/UserService.java
+++ src/main/java/softeer/tenten/service/user/UserService.java
@@ -18,16 +18,20 @@ public class UserService {
     private final UserRepository userRepository;
 
     public UserResponse.Login login(UserRequest.Login login){
-        Optional<User> user = userRepository.findUserByUserId(login.getUserId());
+        User user = getUserByUserId(login.getUserId());
 
         if(isLoginFailure(user, login.getPassword())){
             throw new GeneralException(StatusCode.NOT_FOUND);
         }
 
-        return UserMapper.toLogin(user.get());
+        return UserMapper.toLogin(user);
     }
 
-    private boolean isLoginFailure(Optional<User> user, String password){
-        return user.isEmpty() || !user.get().getPassword().equals(password);
+    public User getUserByUserId(String userId){
+        return userRepository.findUserByUserId(userId).orElseThrow(() -> new GeneralException(StatusCode.NOT_FOUND));
+    }
+
+    private boolean isLoginFailure(User user, String password){
+        return !user.getPassword().equals(password);
     }
 }
