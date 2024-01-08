f3c97ababcecfad8e00682ed408ebf77e840b035
LEE, TAEGON <102223636+taegon98@users.noreply.github.com>
2024-01-08 09:28:38 +0900

    refact: 리뷰 전체 조회 Exception handling

--- src/main/java/softeer/tenten/service/review/ReviewService.java
+++ src/main/java/softeer/tenten/service/review/ReviewService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import softeer.tenten.dto.review.ReviewResponse;
 import softeer.tenten.entity.review.Review;
+import softeer.tenten.global.api.status.StatusCode;
+import softeer.tenten.global.exception.GeneralException;
 import softeer.tenten.mapper.review.ReviewMapper;
 import softeer.tenten.repository.review.ReviewRepository;
 
@@ -20,8 +22,13 @@ public class ReviewService {
 
     private final ReviewRepository reviewRepository;
 
+    //리뷰 전체 조회
     public List<ReviewResponse.ReviewList> getReviewList(Long id) {
         Optional<Review> reviews = reviewRepository.findAllByPopupId(id);
+
+        if (reviews.isEmpty()) {
+            throw new GeneralException(StatusCode.NOT_FOUND);
+        }
 
         return reviews.stream()
                 .map(ReviewMapper::toReviewList)
