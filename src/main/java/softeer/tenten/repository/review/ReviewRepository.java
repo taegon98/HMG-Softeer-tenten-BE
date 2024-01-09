8e4247abf2bc6c03b8f6c01eb28869193094b88b
LEE, TAEGON <taegon1998@gmail.com>
2024-01-09 14:55:51 +0900

    fix: Review Repository 반환 형태 수정
    - findAllByPopupId 메소드: Optional -> List

--- src/main/java/softeer/tenten/repository/review/ReviewRepository.java
+++ src/main/java/softeer/tenten/repository/review/ReviewRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import softeer.tenten.entity.review.Review;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    Optional<Review> findAllByPopupId(Long popupId);
+    List<Review> findAllByPopupId(Long popupId);
 }
