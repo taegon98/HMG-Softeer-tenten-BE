881aab118fd60707bbdfd5c0f5677ca83dbd459a
Hymin2 <khm8246@gmail.com>
2024-01-10 11:39:26 +0900

    fix: 투표 결과 조회 시 자신이 투표한 옵션도 응답 데이터로 제공하도록 수정

--- src/main/java/softeer/tenten/dto/vote/VoteResponse.java
+++ src/main/java/softeer/tenten/dto/vote/VoteResponse.java
@@ -4,13 +4,23 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 public class VoteResponse {
     @Getter
     @Builder
     @RequiredArgsConstructor
-    public static class RegisterVote {
+    public static class VoteResult {
         private final Long id;
         private final String name;
         private final Integer result;
+    }
+
+    @Getter
+    @Builder
+    @RequiredArgsConstructor
+    public static class VoteInformation {
+        private final List<VoteResult> voteResults;
+        private final Long myVoteNumber;
     }
 }
--- src/main/java/softeer/tenten/mapper/vote/VoteMapper.java
+++ src/main/java/softeer/tenten/mapper/vote/VoteMapper.java
@@ -6,13 +6,21 @@ import softeer.tenten.entity.vote.Vote;
 import softeer.tenten.entity.user.User;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class VoteMapper {
-    public static VoteResponse.RegisterVote toVoteOption(Option option, Integer result){
-        return VoteResponse.RegisterVote.builder()
+    public static VoteResponse.VoteResult toVoteResult(Option option, Integer result){
+        return VoteResponse.VoteResult.builder()
                 .id(option.getId())
                 .name(option.getName())
                 .result(result)
+                .build();
+    }
+
+    public static VoteResponse.VoteInformation toVoteInformation(List<VoteResponse.VoteResult> voteResults, Long myVoteNumber){
+        return VoteResponse.VoteInformation.builder()
+                .voteResults(voteResults)
+                .myVoteNumber(myVoteNumber)
                 .build();
     }
 
